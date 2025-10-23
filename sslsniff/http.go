16738d72dba97f2d9a7a365c380819d6d017cdc2
Keming <kemingy94@gmail.com>
2025-10-23 18:45:27 +0800

    fix: drop the stream if encounter parse error (#39)
    * fix: drop the stream if encounter parse error
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * log the key when encounter error
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    ---------
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- sslsniff/http.go
+++ sslsniff/http.go
@@ -225,7 +225,7 @@ func (s *SSLStore) parseRequest(channel chan *watchu.TableRequest) {
 		}
 		request, consumed, err := parser.ParseRequest(record)
 		if err != nil {
-			log.Error().Str("body", string(record.Stream[:consumed])).Err(err).Msg("failed to parse HTTP request")
+			log.Error().Any("key", &key).Bytes("buf", record.Stream[:consumed]).Err(err).Msg("failed to parse HTTP request")
 		}
 		// wait for more data
 		if consumed == 0 || !record.EndOfStream {
@@ -272,7 +272,7 @@ func (s *SSLStore) parseRequest(channel chan *watchu.TableRequest) {
 		}
 		body, err := readDecodeBytes(request.Body, request.Header.Get("Content-Encoding"))
 		if err != nil {
-			log.Error().Err(err).Msg("failed to read request body")
+			log.Error().Any("key", &key).Err(err).Msg("failed to read request body")
 		}
 		url := request.RequestURI
 		if request.URL != nil {
@@ -311,7 +311,7 @@ func (s *SSLStore) parseResponse(channel chan *watchu.TableResponse) {
 		for len(record.Stream) > 0 {
 			response, consumed, err := parser.ParseResponse(record)
 			if err != nil {
-				log.Error().Err(err).Msg("failed to parse HTTP response")
+				log.Error().Any("key", &key).Bytes("buf", record.Stream[:consumed]).Err(err).Msg("failed to parse HTTP response")
 			}
 			// wait for more data
 			if consumed == 0 {
@@ -326,7 +326,7 @@ func (s *SSLStore) parseResponse(channel chan *watchu.TableResponse) {
 			} else {
 				// response won't exceed the max size, see github issue #17
 				if consumed > len(record.Stream) {
-					log.Error().Int("consumed", consumed).Int("stream_len", len(record.Stream)).Bytes("stream", record.Stream).Msg("consumed length exceeds stream length")
+					log.Error().Any("key", &key).Int("consumed", consumed).Int("stream_len", len(record.Stream)).Bytes("stream", record.Stream).Msg("consumed length exceeds stream length")
 					consumed = len(record.Stream)
 				}
 				record.Stream = record.Stream[consumed:]
@@ -361,7 +361,7 @@ func (s *SSLStore) parseResponse(channel chan *watchu.TableResponse) {
 				}
 				body, err := readDecodeBytes(response.Body, response.Header.Get("Content-Encoding"))
 				if err != nil {
-					log.Error().Err(err).Msg("failed to read response body")
+					log.Error().Any("key", &key).Err(err).Msg("failed to read response body")
 				}
 				log.Info().Uint64("timestamp", timestamp).Str("comm", comm).Int("len", consumed).Any("headers", response.Header).Int64("content_length", response.ContentLength).Int("status_code", response.StatusCode).Str("protocol", response.Proto).Bytes("body", body).Bool("truncated", false).Msg("")
 				record.EndOfStream = false
@@ -420,7 +420,7 @@ func (h1 *HTTP1Parser) ParseRequest(record *SSLRecord) (*http.Request, int, erro
 			return nil, idx + HTTP1_DELIMITER_LEN, err
 		}
 		// have to throw away to avoid infinite loop
-		return nil, 0, err
+		return nil, len(record.Stream), err
 	}
 	// find the end of the body
 	idx := bytes.Index(record.Stream, HTTP1DELIMITER)
@@ -498,7 +498,7 @@ func (h1 *HTTP1Parser) ParseResponse(record *SSLRecord) (*http.Response, int, er
 			return nil, idx + HTTP1_DELIMITER_LEN, err
 		}
 		// have to throw away to avoid infinite loop
-		return nil, 0, err
+		return nil, len(record.Stream), err
 	}
 	// find the end of the header
 	idx := bytes.Index(record.Stream, HTTP1DELIMITER)
@@ -613,10 +613,10 @@ func (h2 *HTTP2Parser) parse(record *SSLRecord) (headers []hpack.HeaderField, bo
 			record.EndOfStream = true
 		default:
 			// ignore other connection-level frames
-			log.Trace().Bool("EOS", record.EndOfStream).Any("info", record.Info).Str("frame", fmt.Sprintf("%T", f)).Msg("ignoring non-header/data frame")
+			log.Trace().Bool("EOS", record.EndOfStream).Any("info", &record.Info).Str("frame", fmt.Sprintf("%T", f)).Msg("ignoring non-header/data frame")
 		}
 		lastPos += int(frame.Header().Length) + HTTP2_FRAME_HEADER_LEN
-		log.Trace().Bool("EOS", record.EndOfStream).Any("info", record.Info).Int("lastPos", lastPos).Any("type", frame).Msg("parsed another HTTP/2 frame")
+		log.Trace().Bool("EOS", record.EndOfStream).Any("info", &record.Info).Int("lastPos", lastPos).Any("type", frame).Msg("parsed another HTTP/2 frame")
 	}
 	return
 }

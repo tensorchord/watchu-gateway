d13701147650a1acabcb402040054d8f553fb652
xieydd <xieydd@gmail.com>
2025-12-23 19:58:54 +0800

    k8s: debug, now it works
    Signed-off-by: xieydd <xieydd@gmail.com>

--- collector/internal/logger/log.go
+++ collector/internal/logger/log.go
@@ -1,21 +1,27 @@
 package logger
 
 import (
-	"os"
+	"time"
 
 	"github.com/phuslu/log"
 )
 
-func SetUpLogger() {
-	// Always use console writer for containers to ensure logs go to stdout
-	log.DefaultLogger = log.Logger{
-		Level:      log.DebugLevel,
-		TimeFormat: "15:04:05.123Z",
-		Caller:     1,
-		Writer: &log.ConsoleWriter{
-			ColorOutput:    log.IsTerminal(os.Stderr.Fd()),
-			QuoteString:    true,
-			EndWithMessage: true,
-		},
+func SetUpLogger(debug bool) {
+	if debug {
+		log.DefaultLogger = log.Logger{
+			Level:  log.DebugLevel,
+			Caller: 1,
+			Writer: &log.ConsoleWriter{
+				ColorOutput:    true,
+				QuoteString:    true,
+				EndWithMessage: true,
+			},
+		}
+	} else {
+		log.DefaultLogger = log.Logger{
+			Level:        log.InfoLevel,
+			TimeField:    "timestamp",
+			TimeLocation: time.UTC,
+		}
 	}
 }

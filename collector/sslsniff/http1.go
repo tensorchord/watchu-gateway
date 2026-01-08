d48d44bad37b41cf3eeb73b134fbad4f03f758dc
Keming <kemingyang@tensorchord.ai>
2026-01-08 10:18:17 +0800

    refactor: split protocol parser into different files
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

---
+++ collector/sslsniff/http1.go
@@ -0,0 +1,136 @@
+package sslsniff
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/phuslu/log"
+)
+
+type HTTP1Parser struct{}
+
+func (h1 *HTTP1Parser) ParseRequest(record *SSLRecord) (*http.Request, int, error) {
+	reader := bytes.NewReader(record.Stream)
+	br := bufio.NewReader(reader)
+	req, err := http.ReadRequest(br)
+	if err != nil {
+		// should wait for more data if it's unexpected EOF
+		if errors.Is(err, io.ErrUnexpectedEOF) {
+			return nil, 0, nil
+		}
+		// trim to the `\r\n\r\n`
+		if idx := bytes.Index(record.Stream, HTTP1DELIMITER); idx != -1 {
+			return nil, idx + HTTP1_DELIMITER_LEN, err
+		}
+		// have to throw away to avoid infinite loop
+		return nil, len(record.Stream), err
+	}
+	// find the end of the body
+	idx := bytes.Index(record.Stream, HTTP1DELIMITER)
+	if idx == -1 {
+		return req, len(record.Stream), fmt.Errorf("cannot find the end of HTTP header")
+	}
+	// check if the body is fully received
+	length_to_consume := idx + HTTP1_DELIMITER_LEN + int(req.ContentLength)
+	if req.ContentLength >= 0 && length_to_consume > len(record.Stream) {
+		// when the data is too large to be handled, returned the truncated body
+		if length_to_consume > SSL_MAX_DATA_SIZE && len(record.Stream)+SSL_MAX_EVENT_SIZE > SSL_MAX_DATA_SIZE {
+			log.Debug().Int("content_length", int(req.ContentLength)).Int("received", len(record.Stream)-idx-HTTP1_DELIMITER_LEN).Msg("truncate HTTP/1 request body")
+			record.EndOfStream = true
+			length_to_consume = min(SSL_MAX_DATA_SIZE, len(record.Stream))
+			return req, length_to_consume, nil
+		}
+		// wait for more data, do not return the half-received request body
+		log.Debug().Int("content_length", int(req.ContentLength)).Int("received", len(record.Stream)-idx-HTTP1_DELIMITER_LEN).Msg("incomplete HTTP request body, wait for more data")
+		record.EndOfStream = false
+		return nil, 0, nil
+	}
+	record.EndOfStream = true
+	return req, length_to_consume, nil
+}
+
+func parseStream(data []uint8) ([]uint8, uint64, error) {
+	if idx := bytes.Index(data, CRLF); idx != -1 {
+		length, err := strconv.ParseUint(string(data[:idx]), 16, 64)
+		if err != nil {
+			return nil, 0, fmt.Errorf("failed to parse stream length: %w", err)
+		}
+		consumed := uint64(idx) + CRLF_LEN + length + CRLF_LEN
+		if consumed > uint64(len(data)) {
+			// wait for more data
+			return nil, 0, nil
+		}
+		return data[idx+CRLF_LEN : idx+CRLF_LEN+int(length)], consumed, nil
+	}
+	return nil, 0, fmt.Errorf("failed to parse stream length: no CRLF found")
+}
+
+func isChunkedEncoding(resp *http.Response) bool {
+	encodingLen := len(resp.TransferEncoding)
+	return encodingLen > 0 && resp.TransferEncoding[encodingLen-1] == ChunkedEncodingValue
+}
+
+func (h1 *HTTP1Parser) ParseResponse(record *SSLRecord) (*http.Response, int, error) {
+	// streaming response, handle the chunked transfer encoding
+	if record.LastResp != nil && isChunkedEncoding(record.LastResp) {
+		stream, consumed, err := parseStream(record.Stream)
+		if err != nil || consumed == 0 {
+			return nil, 0, err
+		}
+		switch consumed {
+		case StreamEndChunkLength:
+			// end of the stream
+			record.EndOfStream = true
+		default:
+			record.EndOfStream = false
+			record.LastResp.Body = io.NopCloser(io.MultiReader(record.LastResp.Body, bytes.NewReader(stream)))
+		}
+		return record.LastResp, int(consumed), nil
+	}
+
+	reader := bytes.NewReader(record.Stream)
+	br := bufio.NewReader(reader)
+	resp, err := http.ReadResponse(br, nil)
+	if err != nil {
+		// should wait for more data if it's unexpected EOF
+		if errors.Is(err, io.ErrUnexpectedEOF) {
+			return nil, 0, nil
+		}
+		// trim to the `\r\n\r\n`
+		if idx := bytes.Index(record.Stream, HTTP1DELIMITER); idx != -1 {
+			return nil, idx + HTTP1_DELIMITER_LEN, err
+		}
+		// have to throw away to avoid infinite loop
+		return nil, len(record.Stream), err
+	}
+	// find the end of the header
+	idx := bytes.Index(record.Stream, HTTP1DELIMITER)
+	if idx == -1 {
+		return resp, len(record.Stream), fmt.Errorf("cannot find the end of HTTP header")
+	}
+	// update the last response
+	record.LastResp = resp
+
+	contentLength := resp.ContentLength
+	// Receiving stream, leave the body for the next round
+	if isChunkedEncoding(resp) {
+		record.EndOfStream = false
+		contentLength = 0
+		resp.Body = io.NopCloser(bytes.NewReader([]byte{})) // change to empty body, so next time will handle the chunk
+	} else {
+		// Non-Streaming response should end here if the body has been fully received
+		consumed := idx + HTTP1_DELIMITER_LEN + int(contentLength)
+		if consumed > len(record.Stream) {
+			// wait for more data
+			log.Debug().Int("consumed", consumed).Int("len_stream", len(record.Stream)).Msg("wait for more data to fill this response")
+			return nil, 0, nil
+		}
+		record.EndOfStream = true
+	}
+	return resp, idx + HTTP1_DELIMITER_LEN + int(contentLength), nil
+}
--- collector/sslsniff/protocol.go
+++ collector/sslsniff/protocol.go
@@ -1,19 +1,14 @@
 package sslsniff
 
 import (
-	"bufio"
 	"bytes"
 	"context"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/binary"
 	"encoding/hex"
-	"errors"
 	"fmt"
-	"io"
 	"net/http"
-	"net/url"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -21,7 +16,6 @@ import (
 
 	"github.com/phuslu/log"
 	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/hpack"
 
 	"github.com/tensorchord/watchu/collector"
 	"github.com/tensorchord/watchu/collector/internal/tool"
@@ -238,7 +232,7 @@ func (s *SSLStore) parseRequest(channel chan *collector.RawRequest) {
 	s.reqMu.Lock()
 	defer s.reqMu.Unlock()
 
-	var parser HTTPParser
+	var parser ProtocolParser
 	for key, record := range s.Request {
 		if isHTTP2Protocol(record.Stream) {
 			parser = s.http2Parser
@@ -251,6 +245,8 @@ func (s *SSLStore) parseRequest(channel chan *collector.RawRequest) {
 		request, consumed, err := parser.ParseRequest(record)
 		if err != nil {
 			log.Error().Any("key", &key).Bytes("buf", record.Stream[:consumed]).Err(err).Msg("failed to parse HTTP request")
+			delete(s.Request, key)
+			continue
 		}
 		// wait for more data
 		if consumed == 0 || !record.EndOfStream {
@@ -298,6 +294,7 @@ func (s *SSLStore) parseRequest(channel chan *collector.RawRequest) {
 		body, err := readDecodeBytes(request.Body, request.Header.Get("Content-Encoding"))
 		if err != nil {
 			log.Error().Any("key", &key).Err(err).Msg("failed to read request body")
+			continue
 		}
 		url := request.RequestURI
 		if request.URL != nil {
@@ -333,7 +330,7 @@ func (s *SSLStore) parseResponse(channel chan *collector.RawResponse) {
 	s.respMu.Lock()
 	defer s.respMu.Unlock()
 
-	var parser HTTPParser
+	var parser ProtocolParser
 	for key, record := range s.Response {
 		if isHTTP2Protocol(record.Stream) {
 			parser = s.http2Parser
@@ -345,6 +342,8 @@ func (s *SSLStore) parseResponse(channel chan *collector.RawResponse) {
 			response, consumed, err := parser.ParseResponse(record)
 			if err != nil {
 				log.Error().Any("key", &key).Bytes("buf", record.Stream[:consumed]).Err(err).Msg("failed to parse HTTP response")
+				delete(s.Response, key)
+				break
 			}
 			// wait for more data
 			if consumed == 0 {
@@ -395,6 +394,7 @@ func (s *SSLStore) parseResponse(channel chan *collector.RawResponse) {
 				body, err := readDecodeBytes(response.Body, response.Header.Get("Content-Encoding"))
 				if err != nil {
 					log.Error().Any("key", &key).Err(err).Msg("failed to read response body")
+					continue
 				}
 				headers := flattenMaskedHeader(response.Header)
 				log.Info().Uint64("timestamp", timestamp).Str("comm", comm).Int("len", consumed).Any("headers", headers).Int64("content_length", response.ContentLength).Int("status_code", response.StatusCode).Str("protocol", response.Proto).Bytes("body", body).Bool("truncated", false).Msg("")
@@ -438,321 +438,7 @@ func (s *SSLStore) Parse(ctx context.Context, reqChan chan *collector.RawRequest
 	}
 }
 
-type HTTPParser interface {
+type ProtocolParser interface {
 	ParseRequest(record *SSLRecord) (*http.Request, int, error)
 	ParseResponse(record *SSLRecord) (*http.Response, int, error)
-}
-
-type HTTP1Parser struct{}
-
-func (h1 *HTTP1Parser) ParseRequest(record *SSLRecord) (*http.Request, int, error) {
-	reader := bytes.NewReader(record.Stream)
-	br := bufio.NewReader(reader)
-	req, err := http.ReadRequest(br)
-	if err != nil {
-		// should wait for more data if it's unexpected EOF
-		if errors.Is(err, io.ErrUnexpectedEOF) {
-			return nil, 0, nil
-		}
-		// trim to the `\r\n\r\n`
-		if idx := bytes.Index(record.Stream, HTTP1DELIMITER); idx != -1 {
-			return nil, idx + HTTP1_DELIMITER_LEN, err
-		}
-		// have to throw away to avoid infinite loop
-		return nil, len(record.Stream), err
-	}
-	// find the end of the body
-	idx := bytes.Index(record.Stream, HTTP1DELIMITER)
-	if idx == -1 {
-		return req, len(record.Stream), fmt.Errorf("cannot find the end of HTTP header")
-	}
-	// check if the body is fully received
-	length_to_consume := idx + HTTP1_DELIMITER_LEN + int(req.ContentLength)
-	if req.ContentLength >= 0 && length_to_consume > len(record.Stream) {
-		// when the data is too large to be handled, returned the truncated body
-		if length_to_consume > SSL_MAX_DATA_SIZE && len(record.Stream)+SSL_MAX_EVENT_SIZE > SSL_MAX_DATA_SIZE {
-			log.Debug().Int("content_length", int(req.ContentLength)).Int("received", len(record.Stream)-idx-HTTP1_DELIMITER_LEN).Msg("truncate HTTP/1 request body")
-			record.EndOfStream = true
-			length_to_consume = min(SSL_MAX_DATA_SIZE, len(record.Stream))
-			return req, length_to_consume, nil
-		}
-		// wait for more data, do not return the half-received request body
-		log.Debug().Int("content_length", int(req.ContentLength)).Int("received", len(record.Stream)-idx-HTTP1_DELIMITER_LEN).Msg("incomplete HTTP request body, wait for more data")
-		record.EndOfStream = false
-		return nil, 0, nil
-	}
-	record.EndOfStream = true
-	return req, length_to_consume, nil
-}
-
-func parseStream(data []uint8) ([]uint8, uint64, error) {
-	if idx := bytes.Index(data, CRLF); idx != -1 {
-		length, err := strconv.ParseUint(string(data[:idx]), 16, 64)
-		if err != nil {
-			return nil, 0, fmt.Errorf("failed to parse stream length: %w", err)
-		}
-		consumed := uint64(idx) + CRLF_LEN + length + CRLF_LEN
-		if consumed > uint64(len(data)) {
-			// wait for more data
-			return nil, 0, nil
-		}
-		return data[idx+CRLF_LEN : idx+CRLF_LEN+int(length)], consumed, nil
-	}
-	return nil, 0, fmt.Errorf("failed to parse stream length: no CRLF found")
-}
-
-func isChunkedEncoding(resp *http.Response) bool {
-	encodingLen := len(resp.TransferEncoding)
-	return encodingLen > 0 && resp.TransferEncoding[encodingLen-1] == ChunkedEncodingValue
-}
-
-func (h1 *HTTP1Parser) ParseResponse(record *SSLRecord) (*http.Response, int, error) {
-	// streaming response, handle the chunked transfer encoding
-	if record.LastResp != nil && isChunkedEncoding(record.LastResp) {
-		stream, consumed, err := parseStream(record.Stream)
-		if err != nil || consumed == 0 {
-			return nil, 0, err
-		}
-		switch consumed {
-		case StreamEndChunkLength:
-			// end of the stream
-			record.EndOfStream = true
-		default:
-			record.EndOfStream = false
-			record.LastResp.Body = io.NopCloser(io.MultiReader(record.LastResp.Body, bytes.NewReader(stream)))
-		}
-		return record.LastResp, int(consumed), nil
-	}
-
-	reader := bytes.NewReader(record.Stream)
-	br := bufio.NewReader(reader)
-	resp, err := http.ReadResponse(br, nil)
-	if err != nil {
-		// should wait for more data if it's unexpected EOF
-		if errors.Is(err, io.ErrUnexpectedEOF) {
-			return nil, 0, nil
-		}
-		// trim to the `\r\n\r\n`
-		if idx := bytes.Index(record.Stream, HTTP1DELIMITER); idx != -1 {
-			return nil, idx + HTTP1_DELIMITER_LEN, err
-		}
-		// have to throw away to avoid infinite loop
-		return nil, len(record.Stream), err
-	}
-	// find the end of the header
-	idx := bytes.Index(record.Stream, HTTP1DELIMITER)
-	if idx == -1 {
-		return resp, len(record.Stream), fmt.Errorf("cannot find the end of HTTP header")
-	}
-	// update the last response
-	record.LastResp = resp
-
-	contentLength := resp.ContentLength
-	// Receiving stream, leave the body for the next round
-	if isChunkedEncoding(resp) {
-		record.EndOfStream = false
-		contentLength = 0
-		resp.Body = io.NopCloser(bytes.NewReader([]byte{})) // change to empty body, so next time will handle the chunk
-	} else {
-		// Non-Streaming response should end here if the body has been fully received
-		consumed := idx + HTTP1_DELIMITER_LEN + int(contentLength)
-		if consumed > len(record.Stream) {
-			// wait for more data
-			log.Debug().Int("consumed", consumed).Int("len_stream", len(record.Stream)).Msg("wait for more data to fill this response")
-			return nil, 0, nil
-		}
-		record.EndOfStream = true
-	}
-	return resp, idx + HTTP1_DELIMITER_LEN + int(contentLength), nil
-}
-
-type HTTP2Parser struct {
-	dec *hpack.Decoder
-}
-
-func NewHTTP2Parser() *HTTP2Parser {
-	return &HTTP2Parser{
-		dec: hpack.NewDecoder(4096, nil),
-	}
-}
-
-func (h2 *HTTP2Parser) parse(record *SSLRecord) (headers []hpack.HeaderField, body bytes.Buffer, lastPos int, err error) {
-	data := record.Stream
-	if bytes.HasPrefix(record.Stream, HTTP2PREFACE) {
-		lastPos += HTTP2PREFACE_LEN
-		data = data[HTTP2PREFACE_LEN:]
-	}
-
-	reader := bytes.NewReader(data)
-	framer := http2.NewFramer(nil, reader)
-	var frame http2.Frame
-	var hbStreamID uint32
-	var hbBuf []byte
-	var hbOpen bool
-
-	for !record.EndOfStream {
-		frame, err = framer.ReadFrame()
-		if err != nil {
-			if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
-				// wait for more data, suppress the EOF error
-				err = nil
-				log.Trace().Int("len", len(data)).Str("buf", hex.EncodeToString(data)).Msg("HTTP/2 parsing reaches EOF")
-				return
-			}
-			err = fmt.Errorf("failed to read HTTP/2 frame: %w", err)
-			return
-		}
-		switch f := frame.(type) {
-		case *http2.HeadersFrame:
-			if hbOpen && hbStreamID != f.Header().StreamID {
-				err = fmt.Errorf("protocol error: new HEADERS on %d while block open on %d", f.Header().StreamID, hbStreamID)
-				return
-			}
-			if !hbOpen {
-				hbOpen = true
-				hbStreamID = f.Header().StreamID
-				hbBuf = hbBuf[:0]
-			}
-			hbBuf = append(hbBuf, f.HeaderBlockFragment()...)
-			if f.HeadersEnded() {
-				var hdrs []hpack.HeaderField
-				hdrs, err = h2.dec.DecodeFull(hbBuf)
-				if err != nil {
-					err = fmt.Errorf("failed to decode HTTP/2 headers: %w", err)
-					return
-				}
-				headers = append(headers, hdrs...)
-				hbOpen = false
-			}
-			if f.StreamEnded() {
-				record.EndOfStream = true
-			}
-		case *http2.ContinuationFrame:
-			if !hbOpen {
-				err = fmt.Errorf("protocol error: unexpected CONTINUATION on %d without HEADER block open", f.Header().StreamID)
-				return
-			}
-			if hbOpen && hbStreamID != f.Header().StreamID {
-				err = fmt.Errorf("protocol error: CONTINUATION on %d while block open on %d", f.Header().StreamID, hbStreamID)
-				return
-			}
-			hbBuf = append(hbBuf, f.HeaderBlockFragment()...)
-			if f.HeadersEnded() {
-				var hdrs []hpack.HeaderField
-				hdrs, err = h2.dec.DecodeFull(hbBuf)
-				if err != nil {
-					err = fmt.Errorf("failed to decode HTTP/2 headers: %w", err)
-					return
-				}
-				headers = append(headers, hdrs...)
-				hbOpen = false
-			}
-		case *http2.DataFrame:
-			body.Write(f.Data())
-			if f.StreamEnded() {
-				record.EndOfStream = true
-			}
-		case *http2.RSTStreamFrame:
-			record.EndOfStream = true
-		case *http2.GoAwayFrame:
-			record.EndOfStream = true
-		default:
-			// ignore other connection-level frames
-			log.Trace().Bool("EOS", record.EndOfStream).Any("info", &record.Info).Str("frame", fmt.Sprintf("%T", f)).Msg("ignoring non-header/data frame")
-		}
-		lastPos += int(frame.Header().Length) + HTTP2_FRAME_HEADER_LEN
-		log.Trace().Bool("EOS", record.EndOfStream).Any("info", &record.Info).Int("lastPos", lastPos).Any("type", frame).Msg("parsed another HTTP/2 frame")
-	}
-	return
-}
-
-func (h2 *HTTP2Parser) ParseRequest(record *SSLRecord) (*http.Request, int, error) {
-	headers, body, lastPos, err := h2.parse(record)
-	if err != nil {
-		return nil, lastPos, err
-	}
-
-	if !record.EndOfStream && lastPos+SSL_MAX_EVENT_SIZE > SSL_MAX_DATA_SIZE {
-		// truncate the request body
-		log.Debug().Int("lastPos", lastPos).Msg("truncate HTTP/2 request body")
-		record.EndOfStream = true
-	}
-	if !record.EndOfStream {
-		// wait for more data
-		return nil, 0, nil
-	}
-	// could be a GoAwayFrame, no need to record this one
-	if len(headers) == 0 && body.Len() == 0 {
-		return nil, lastPos, nil
-	}
-	// convert headers to http.Request
-	hdrs := http.Header{}
-	var method, scheme, path, authority string
-	for _, hf := range headers {
-		switch hf.Name {
-		case ":method":
-			method = hf.Value
-		case ":scheme":
-			scheme = hf.Value
-		case ":path":
-			path = hf.Value
-		case ":authority":
-			authority = hf.Value
-		default:
-			hdrs.Add(hf.Name, hf.Value)
-		}
-	}
-	url := &url.URL{
-		Scheme: scheme,
-		Host:   authority,
-		Path:   path,
-	}
-	req := &http.Request{
-		Method:     method,
-		URL:        url,
-		Header:     hdrs,
-		Host:       authority,
-		Proto:      "HTTP/2.0",
-		ProtoMajor: 2,
-		ProtoMinor: 0,
-		Body:       io.NopCloser(&body),
-	}
-	return req, lastPos, nil
-}
-
-func (h2 *HTTP2Parser) ParseResponse(record *SSLRecord) (*http.Response, int, error) {
-	headers, body, lastPos, err := h2.parse(record)
-	if err != nil {
-		return nil, lastPos, err
-	}
-
-	// create a new Response
-	if record.LastResp == nil {
-		// convert headers to http.Response
-		hdrs := http.Header{}
-		var code int
-		for _, hf := range headers {
-			switch hf.Name {
-			case ":status":
-				code, err = strconv.Atoi(hf.Value)
-				if err != nil {
-					return nil, lastPos, fmt.Errorf("invalid status code: %s", hf.Value)
-				}
-			default:
-				hdrs.Add(hf.Name, hf.Value)
-			}
-		}
-		record.LastResp = &http.Response{
-			Status:     fmt.Sprintf("%d %s", code, http.StatusText(code)),
-			StatusCode: code,
-			Header:     hdrs,
-			Proto:      "HTTP/2.0",
-			ProtoMajor: 2,
-			ProtoMinor: 0,
-			Body:       io.NopCloser(&body),
-		}
-	} else {
-		record.LastResp.Body = io.NopCloser(io.MultiReader(record.LastResp.Body, &body))
-	}
-	return record.LastResp, lastPos, nil
 }
